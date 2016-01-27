a44b050dc0305a5694117beb316eea7aff8b7d36
kiran4399 <kiran4399@gmail.com>
2016-01-27 16:01:03 +0530

    bfs added

--- 8puzzle_bfs.cpp
+++ 8puzzle_bfs.cpp
@@ -7,15 +7,18 @@ using namespace std;
 /*This is program to go from a start state of an 8-Puzzle problem to a goal state through DFS.
 Give input with space between each number and give input as 0 for the blank space*/
 
-int blank_xloc=0, blank_yloc=0;
+int index=0;
 
 struct state
 {
 	int matrix[3][3];
+	int blank_xloc;
+	int blank_yloc;
 };
 
-stack<struct state> open;
+struct state visited[10000];
 queue<struct state> closed;
+
 
 int check_cycle(struct state cur)
 {
@@ -53,11 +56,12 @@ int cmp_cur_goal (struct state cur, struct state goal)
 
 int move_left(struct state cur, struct state goal)
 {
-	if (blank_yloc > 0)
+	if (cur.blank_yloc > 0)
 	{
-		cur.matrix[blank_xloc][blank_yloc] = cur.matrix[blank_xloc][blank_yloc-1];
-		cur.matrix[blank_xloc][blank_yloc-1] = 0;
-		blank_yloc -= 1;
+		cur.matrix[cur.blank_xloc][cur.blank_yloc] = cur.matrix[cur.blank_xloc][cur.blank_yloc-1];
+		cur.matrix[cur.blank_xloc][cur.blank_yloc-1] = 0;
+		cur.blank_yloc -= 1;
+		closed.push(cur);
 		return 1;
 	}
 	return 0;
@@ -65,11 +69,12 @@ int move_left(struct state cur, struct state goal)
 
 int move_down(struct state cur, struct state goal)
 {
-	if (blank_xloc < 2)
+	if (cur.blank_xloc < 2)
 	{
-		cur.matrix[blank_xloc][blank_yloc] = cur.matrix[blank_xloc+1][blank_yloc];
-		cur.matrix[blank_xloc+1][blank_yloc] = 0;
-		blank_xloc += 1;
+		cur.matrix[cur.blank_xloc][cur.blank_yloc] = cur.matrix[cur.blank_xloc+1][cur.blank_yloc];
+		cur.matrix[cur.blank_xloc+1][cur.blank_yloc] = 0;
+		cur.blank_xloc += 1;
+		closed.push(cur);
 		return 1;
 	}
 	return 0;
@@ -77,11 +82,12 @@ int move_down(struct state cur, struct state goal)
 
 int move_right(struct state cur, struct state goal)
 {
-	if (blank_yloc < 2)
+	if (cur.blank_yloc < 2)
 	{
-		cur.matrix[blank_xloc][blank_yloc] = cur.matrix[blank_xloc][blank_yloc+1];
-		cur.matrix[blank_xloc][blank_yloc+1] = 0;
-		blank_yloc += 1;
+		cur.matrix[cur.blank_xloc][cur.blank_yloc] = cur.matrix[cur.blank_xloc][cur.blank_yloc+1];
+		cur.matrix[cur.blank_xloc][cur.blank_yloc+1] = 0;
+		cur.blank_yloc += 1;
+		closed.push(cur);
 		return 1;
 	}
 	return 0;
@@ -89,23 +95,52 @@ int move_right(struct state cur, struct state goal)
 
 int move_up(struct state cur, struct state goal)
 {
-	if (blank_xloc > 0)
+	if (cur.blank_xloc > 0)
 	{
-		cur.matrix[blank_xloc][blank_yloc] = cur.matrix[blank_xloc-1][blank_yloc];
-		cur.matrix[blank_xloc-1][blank_yloc] = 0;
-		blank_xloc -= 1;
+		cur.matrix[cur.blank_xloc][cur.blank_yloc] = cur.matrix[cur.blank_xloc-1][cur.blank_yloc];
+		cur.matrix[cur.blank_xloc-1][cur.blank_yloc] = 0;
+		cur.blank_xloc -= 1;
+		closed.push(cur);
 		return 1;
 	}
 	return 0;
 	
 }
 
+void print(struct state temp){
 
-int dfs(struct state cur, struct state goal)
+	for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				cout<<temp.matrix[i][j]<<' ';
+			}
+			cout<<endl;
+		}
+}
+int check_visited (struct state cur, int index)
 {
+	for (int i=0; i<index; i++)
+	{
+		int val = cmp_cur_goal(visited[i], cur);
+		if(val==1)
+			return i;
+	}
+	return -1;
+}
+
+int bfs(struct state goal)
+{
+	int flag=0;
 	getchar();
-	check_cycle(cur);
-	closed.push(cur);
+	//check_cycle(cur);
+	//queue<struct state> Q;
+	struct state cur = closed.front();
+	print(cur);
+	closed.pop();
+
+	if(check_visited(cur, index)!=-1)
+		return bfs(goal);
 
 	int cmp_val = cmp_cur_goal(cur,goal);
 	if (cmp_val == 1)
@@ -115,21 +150,31 @@ int dfs(struct state cur, struct state goal)
 	moved = move_left(cur, goal);
 	if (moved == 1)
 	{
-		return dfs (cur, goal);
+		flag=1;
 	}
 	moved = move_down(cur, goal);
 	if (moved == 1)
 	{
-		return dfs (cur, goal);
+		flag=1;
+		//cout << "1" << endl;
 	}
 	moved = move_right(cur, goal);
 	if (moved == 1)
 	{
-		return dfs (cur, goal);
+		flag=1;
 	}	
 	moved = move_up(cur, goal);
 	if(moved == 1)
-		return dfs (cur, goal);
+	{	
+		flag=1;
+		//return bfs (cur, goal);
+	}
+
+	if(flag == 1){
+		cout << "3" << endl;
+		visited[index++]=cur;
+		return bfs(goal);
+	}
 	else
 		return 0;
 }
@@ -145,8 +190,8 @@ int main()
 			cin>>start.matrix[i][j];
 			if(start.matrix[i][j] == 0)
 			{
-				blank_xloc = i;
-				blank_yloc = j;
+				start.blank_xloc = i;
+				start.blank_yloc = j;
 			}	
 		}
 	}
@@ -156,8 +201,8 @@ int main()
 			cin>>goal.matrix[i][j];
 	}
 
-	open.push(start);
-	if( dfs(start, goal) == 1 )
+	closed.push(start);
+	if( bfs(goal) == 1 )
 		cout<<"Success\n";
 	else
 		cout<<"Failure\n";
